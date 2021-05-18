40c37f06a52d42ce72cac65c9313f5ca86a5572e
Bhaskar V A <bhaskarva9686@gmail.com>
2021-05-18 10:55:29 +0530

    testing

--- src/main/java/com/capgemini/librarymanagementsystem/beans/Users.java
+++ src/main/java/com/capgemini/librarymanagementsystem/beans/Users.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+// Users Class contains information about users
 @Entity
 @Table(name = "users_info")
 public class Users {
